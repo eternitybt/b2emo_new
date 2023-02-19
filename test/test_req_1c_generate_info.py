80a4d53d621425263e21635c9f442f966ebb11d8
Markus Flaig <my.flaig@gmail.com>
2023-02-19 18:55:05 +0000

    Changes to be committed: 	modified:   test/depth_maps/comp.txt 	modified:   test/test_req_1c_generate_info.py

--- test/test_req_1c_generate_info.py
+++ test/test_req_1c_generate_info.py
@@ -2,7 +2,7 @@ import cv2
 from depth_estimation import *
 
 # MAIN.
-for idx_str in ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09']:
+for idx_str in ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10']:
     depth_map = cv2.imread('depth_maps/depth' + idx_str + '.jpg')
     print_map(depth_map, idx_str)
     depth_map_eval = sample(depth_map)
